5dfd99955ce949d9d52209ede237dc6cea200e40
kratel <santi.f.salas@gmail.com>
2019-10-18 04:20:11 -0400

    reorgnized files for autograder, and added style sheet as well as some new placeholder routes.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask('spellchecker', instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'spellchecker.sqlite'),
@@ -24,10 +24,16 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import db
+    import db
     db.init_app(app)
 
-    from . import auth
+    import auth
     app.register_blueprint(auth.bp)
 
+    import spellcheck
+    app.register_blueprint(spellcheck.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
+
+app = create_app()
