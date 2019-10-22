1ee2a1f0fa91804a39f24ddb48e79601e64f5c07
kratel <santi.f.salas@gmail.com>
2019-10-22 01:11:15 -0400

    Swapped spell_check page to use wtforms.

--- spellcheck.py
+++ spellcheck.py
@@ -1,20 +1,29 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+	Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
 
 from auth import login_required
 from db import get_db
+import forms
 
 bp = Blueprint('spellcheck', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('spellcheck/index.html')
+	return render_template('spellcheck/index.html')
 
 
-@bp.route('/spell_check')
+@bp.route('/spell_check', methods=('GET', 'POST'))
 @login_required
 def spell_check():
-    return render_template('spellcheck/spell_check.html')
+	form = forms.SpellCheckForm()
+	if form.validate_on_submit():
+		inputtext = form.inputtext.data
+		error = None
+
+		if error is None:
+			return redirect(url_for('spellcheck.spell_check'))
+
+	return render_template('spellcheck/spell_check.html', form=form)
