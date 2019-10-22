9aa9c5bba6323a4b6f2a8dc528fb9b5d0702bc40
kratel <santi.f.salas@gmail.com>
2019-10-22 00:09:24 -0400

    Setup wtforms.

---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Regexp, Optional
+
+class AuthForm(FlaskForm):
+	username = StringField(label='Username', id='uname', validators=[DataRequired(), Regexp(regex='^(?=.{8,20}$)[a-zA-Z0-9.]+$', message='Registration failure. Invalid char in username or not between 8 - 20 chars')])
+	password = PasswordField(label='Password', id='pword', validators=[DataRequired(), Regexp(regex='^(?=.{8,20}$)[a-zA-Z0-9._$%&*#@!]+$', message='Registration failure. Invalid char in password or not between 8 - 20 chars')])
+	mfa = IntegerField(label='2FA', id='2fa', validators=[Optional()])
