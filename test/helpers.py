75152674d7f70b28d20697ff354d0df5a375e56f
kratel <santi.f.salas@gmail.com>
2019-10-27 12:35:37 -0400

    Updated helpers to build different dicts depending on whether mfa was included.

--- test/helpers.py
+++ test/helpers.py
@@ -1,24 +1,44 @@
 def register(app, uname, pword, mfa="", csrf_token=""):
-    pdata = { "username": uname,
-              "password": pword,
-              "mfa": mfa,
-              "csrf_token": csrf_token}
-    return app.post(
-        '/register',
-        data=pdata,
-        follow_redirects=True
-    )
+    if mfa:
+        pdata = { "username": uname,
+                  "password": pword,
+                  "mfa": mfa,
+                  "csrf_token": csrf_token}
+        return app.post(
+            '/register',
+            data=pdata,
+            follow_redirects=True
+        )
+    else:
+        pdata = { "username": uname,
+                  "password": pword,
+                  "csrf_token": csrf_token}
+        return app.post(
+            '/register',
+            data=pdata,
+            follow_redirects=True
+        )
  
 def login(app, uname, pword, mfa="", csrf_token=""):
-    pdata = { "username": uname,
-              "password": pword,
-              "mfa": mfa,
-              "csrf_token": csrf_token}
-    return app.post(
-        '/login',
-        data=pdata,
-        follow_redirects=True
-    )
+    if mfa:
+        pdata = { "username": uname,
+                  "password": pword,
+                  "mfa": mfa,
+                  "csrf_token": csrf_token}
+        return app.post(
+            '/login',
+            data=pdata,
+            follow_redirects=True
+        )
+    else:
+        pdata = { "username": uname,
+                  "password": pword,
+                  "csrf_token": csrf_token}
+        return app.post(
+            '/login',
+            data=pdata,
+            follow_redirects=True
+        )
  
 def logout(app):
     return app.get(
