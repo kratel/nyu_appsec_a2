28b66bf12e5b0d0d9b20a2c48516d109857b5767
kratel <santi.f.salas@gmail.com>
2019-10-27 04:33:11 -0400

    Add registration tests.

--- test/test_auth.py
+++ test/test_auth.py
@@ -35,6 +35,67 @@ class TestAuth(unittest.TestCase):
 		self.assertGreater(len(soup.find_all('input', id='pword')), 0, "No input with id 'pword' found.")
 		self.assertGreater(len(soup.find_all('input', id='2fa')), 0, "No input with id '2fa' found.")
 
+	def test_register_data_req_validation(self):
+		# Make a GET to grab csrf token
+		response = self.app.get('/register', follow_redirects=True)
+		soup = beautifulsoup(response.data, 'html.parser')
+		csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+		# Test Data Required Validators
+		response = register(app=self.app, uname='', pword='', mfa='', csrf_token='')
+		self.assertEqual(response.status_code, 200)
+		soup = beautifulsoup(response.data, 'html.parser')
+		results = soup.find_all(id='success')
+		self.assertGreater(len(results), 0, "No flash messages received")
+		self.assertTrue(any(("This field is required" in s.text) and ("Username" in s.text) for s in results))
+		self.assertTrue(any(("This field is required" in s.text) and ("Password" in s.text) for s in results))
+		self.assertTrue(any(("The CSRF token is missing" in s.text) for s in results))
+
+	def test_register_data_regex_validation(self):
+		# Make a GET to grab csrf token
+		response = self.app.get('/register', follow_redirects=True)
+		soup = beautifulsoup(response.data, 'html.parser')
+		csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+		# Test Regex Validators
+		response = register(app=self.app, uname='temp^1234', pword='temp123(<4', mfa='', csrf_token=csrf_token)
+		self.assertEqual(response.status_code, 200)
+		soup = beautifulsoup(response.data, 'html.parser')
+		results = soup.find_all(id='success')
+		self.assertTrue(any(("Registration failure" in s.text) for s in results))
+		self.assertTrue(any(("Invalid char in password or not between 8 - 20 chars" in s.text) for s in results))
+		self.assertTrue(any(("Invalid char in username or not between 8 - 20 chars" in s.text) for s in results))
+
+	def test_register_csrf(self):
+		response = self.app.get('/register', follow_redirects=True)
+		self.assertEqual(response.status_code, 200)
+		soup = beautifulsoup(response.data, 'html.parser')
+		self.assertGreater(len(soup.find_all('input', id='csrf_token')), 0, "No csrf token found in input.")
+
+	def test_valid_user_registration(self):
+		response = self.app.get('/register', follow_redirects=True)
+		soup = beautifulsoup(response.data, 'html.parser')
+		csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+		response = register(app=self.app, uname='temp1234', pword='temp1234', mfa='1234', csrf_token=csrf_token)
+		self.assertEqual(response.status_code, 200)
+		soup = beautifulsoup(response.data, 'html.parser')
+		results = soup.find_all(id='success')
+		self.assertGreater(len(results), 0, "No flash messages received")
+		self.assertTrue(any("Registration success" in s.text for s in results))
+
+	def test_repeated_username(self):
+		response = self.app.get('/register', follow_redirects=True)
+		soup = beautifulsoup(response.data, 'html.parser')
+		csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+		response = register(app=self.app, uname='temp1234', pword='temp1234', mfa='1234', csrf_token=csrf_token)
+		# Grab csrf token and try to register with the same username
+		soup = beautifulsoup(response.data, 'html.parser')
+		csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+		response = register(app=self.app, uname='temp1234', pword='temp1234', mfa='1234', csrf_token=csrf_token)
+		self.assertEqual(response.status_code, 200)
+		soup = beautifulsoup(response.data, 'html.parser')
+		results = soup.find_all(id='success')
+		self.assertTrue(any(("Username is not available" in s.text) for s in results))
+		self.assertTrue(any(("Registration failure" in s.text) for s in results))
+
 
 if __name__ == '__main__':
 	unittest.main()
