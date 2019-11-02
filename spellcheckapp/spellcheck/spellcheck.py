99bdd2e0b2c7a02b425c92d56ceb02fd1f2386d3
kratel <santi.f.salas@gmail.com>
2019-11-02 18:12:17 -0400

    Add query history view functionality, not query reviews yet.

--- spellcheckapp/spellcheck/spellcheck.py
+++ spellcheckapp/spellcheck/spellcheck.py
@@ -3,9 +3,10 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
+from spellcheckapp import db
 from spellcheckapp.auth.auth import login_required
-from spellcheckapp.db import get_db
-from spellcheckapp.spellcheck import forms
+from spellcheckapp.auth import models as authmodels
+from spellcheckapp.spellcheck import forms, models
 from shlex import quote
 import subprocess
 import tempfile
@@ -30,7 +31,7 @@ def spell_check():
     results = {}
     if form.validate_on_submit():
         inputtext = quote(form.inputtext.data)
-        inputtext += "\r\n"
+        inputtext += " "
         error = None
 
         if not inputtext:
@@ -40,6 +41,7 @@ def spell_check():
         inputtext = bytes(inputtext, 'utf-8')
         results["textout"] = inputtext.decode()
 
+        new_spell_check = None
         if error is None:
             result = None
             with tempfile.NamedTemporaryFile() as inputfile:
@@ -55,8 +57,14 @@ def spell_check():
             result = list(filter(None, result))
             if result:
                 results["misspelled"] = ", ".join(result)
+                new_spell_check = models.Spell_checks(username=g.user.username, submitted_text=results["textout"], misspelled_words=results["misspelled"])
             else:
                 results["no_misspelled"] = "No misspelled words were found."
+                new_spell_check = models.Spell_checks(username=g.user.username, submitted_text=results["textout"], misspelled_words=results["no_misspelled"])
+
+        if new_spell_check is not None:
+            db.session.add(new_spell_check)
+            db.session.commit()
 
     render = make_response(render_template('spellcheck/spell_check.html', form=form, results=results))
     render.headers.set('Content-Security-Policy', "default-src 'self'")
@@ -64,3 +72,45 @@ def spell_check():
     render.headers.set('X-Frame-Options', 'SAMEORIGIN')
     render.headers.set('X-XSS-Protection', '1; mode=block')
     return render
+
+@bp.route('/history', methods=('GET', 'POST'))
+@login_required
+def history():
+    render = None
+    form = forms.UserHistoryForm()
+    results = {}
+    username = g.user.username
+    queryhistory = models.Spell_checks.query.filter_by(username=username)
+    numqueries = queryhistory.count()
+
+    if g.user.is_admin:
+        if form.validate_on_submit():
+            quser = form.userquery.data
+            error = None
+
+            if not quser:
+                error = "Invalid input"
+                flash(error)
+            elif authmodels.User.query.filter_by(username=quser).first() is None:
+                error = "No user with this username found"
+                flash(error)
+
+            if error is None:
+                username = quser
+                queryhistory = models.Spell_checks.query.filter_by(username=username)
+                numqueries = queryhistory.count()
+
+    if g.user.is_admin:
+        render = make_response(render_template('spellcheck/history.html', form=form, numqueries=numqueries, queryhistory=queryhistory, username=username))
+    else:
+        render = make_response(render_template('spellcheck/history.html', numqueries=numqueries, queryhistory=queryhistory, username=username))
+    render.headers.set('Content-Security-Policy', "default-src 'self'")
+    render.headers.set('X-Content-Type-Options', 'nosniff')
+    render.headers.set('X-Frame-Options', 'SAMEORIGIN')
+    render.headers.set('X-XSS-Protection', '1; mode=block')
+    return render
+
+@bp.route('/history/query<queryid>', methods=['GET'])
+@login_required
+def query(queryid):
+    abort(404)
