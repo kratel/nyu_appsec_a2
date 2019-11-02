3839780a9bc8b19aa0dd0b47efddd79a2a9ade49
kratel <santi.f.salas@gmail.com>
2019-11-02 17:50:39 -0400

    Inc max length to account for whitespace that is added.

--- spellcheckapp/spellcheck/models.py
+++ spellcheckapp/spellcheck/models.py
@@ -12,8 +12,8 @@ CREATE TABLE mfa (
 class Spell_checks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=False, nullable=False)
-    submitted_text = db.Column(db.String(500), unique=False, nullable=False)
-    misspelled_words = db.Column(db.String(500), unique=False, nullable=True)
+    submitted_text = db.Column(db.String(501), unique=False, nullable=False)
+    misspelled_words = db.Column(db.String(501), unique=False, nullable=True)
 
     def __repr__(self):
         return '<User %r Spell_check_id %r>' % (self.username, self.id)
