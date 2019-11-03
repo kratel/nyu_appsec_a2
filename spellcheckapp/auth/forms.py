b381e741e2480ea60200a4404215bbbb78196d43
kratel <santi.f.salas@gmail.com>
2019-11-02 22:17:12 -0400

    Made new form for user auth log queries.

--- spellcheckapp/auth/forms.py
+++ spellcheckapp/auth/forms.py
@@ -7,3 +7,7 @@ class AuthForm(FlaskForm):
 	username = StringField(label='Username', id='uname', validators=[DataRequired(), Regexp(regex='^(?=.{5,20}$)[a-zA-Z0-9._]+$', message='Auth failure. Invalid char in username or not between 5 - 20 chars')])
 	password = PasswordField(label='Password', id='pword', validators=[DataRequired(), Regexp(regex='^(?=.{8,20}$)[a-zA-Z0-9._$%&*#@!]+$', message='Auth failure. Invalid char in password or not between 8 - 20 chars')])
 	mfa = StringField(label='2FA', id='2fa', validators=[Optional(), Length(max=20)])
+
+
+class UserAuthHistoryForm(FlaskForm):
+	userid = IntegerField(label="User ID to query", id='userid', validators=[DataRequired()])
