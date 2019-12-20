49e6177c590891c597a62b45cb7e493f12c348c9
kratel <santi.f.salas@gmail.com>
2019-12-20 03:01:56 -0500

    Linted spellcheck module models.py

--- spellcheckapp/spellcheck/models.py
+++ spellcheckapp/spellcheck/models.py
@@ -1,11 +1,19 @@
+"""Defines Data Models for Spellcheck Module."""
 from spellcheckapp import db
-from spellcheckapp.auth.models import User
+
 
 class Spell_checks(db.Model):
+    """
+    Spell_checks Database Model.
+
+    Defines Spell_checks fields.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), db.ForeignKey('user.username'), unique=False, nullable=False)
     submitted_text = db.Column(db.String(501), unique=False, nullable=False)
     misspelled_words = db.Column(db.String(501), unique=False, nullable=True)
 
     def __repr__(self):
+        """Defines string representation of a Spell_checks tuple."""
         return '<User %r Spell_check_id %r>' % (self.username, self.id)
