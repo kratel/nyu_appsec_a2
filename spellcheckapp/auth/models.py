74eb9327b25e87c0b4b331480b677516126a5943
kratel <santi.f.salas@gmail.com>
2019-12-20 02:40:44 -0500

    Linted auth module models.py file.

--- spellcheckapp/auth/models.py
+++ spellcheckapp/auth/models.py
@@ -1,7 +1,14 @@
+"""Defines Data Models for Auth Module."""
 from spellcheckapp import db
 
 
 class User(db.Model):
+    """
+    User Database Model.
+
+    Defines User fields.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=False, nullable=False)
@@ -9,23 +16,39 @@ class User(db.Model):
     is_admin = db.Column(db.Boolean, unique=False, default=False)
 
     def __repr__(self):
+        """Defines string representation of a User tuple."""
         return '<User %r>' % self.username
 
 
 class MFA(db.Model):
+    """
+    MFA Database Model.
+
+    Defines MFA fields with a foreign key constraint dependent on the User model.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), db.ForeignKey('user.username'), nullable=False)
     mfa_number = db.Column(db.String(20), unique=False, nullable=False)
 
     def __repr__(self):
+        """Defines string representation of a MFA tuple."""
         return '<MFA_User %r' % self.username
 
 
 class AuthLog(db.Model):
+    """
+    AuthLog Database Model.
+
+    Defines AuthLog fields with a foreign key constraint dependent on the User model.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('user.id'))
     username = db.Column(db.String(20), db.ForeignKey('user.username'), nullable=False)
     login_time = db.Column(db.DateTime(), nullable=False)
     logout_time = db.Column(db.DateTime(), nullable=True)
+    
     def __repr__(self):
+        """Defines string representation of an AuthLog tuple."""
         return '<AuthLog %r' % self.id
