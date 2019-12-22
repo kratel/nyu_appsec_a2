e2442c2039aa5fabe5b894c903d23bc1c7833ea7
kratel <santi.f.salas@gmail.com>
2019-12-22 07:05:02 -0500

    Added test for admin view of spell history page and admin functionality on that page. Also added a helper function for the form submission.

--- test/test_spellcheck.py
+++ test/test_spellcheck.py
@@ -110,6 +110,19 @@ class TestAuth(unittest.TestCase):
             follow_redirects=True
         )
 
+    def query_user_spell_history(self, userquery=None, csrf_token=""):
+        """Helper function to issue a request to view spell check history."""
+        if userquery:
+            pdata = {"userquery": userquery,
+                     "csrf_token": csrf_token}
+        else:
+            pdata = {"csrf_token": csrf_token}
+        return self.app.post(
+            '/history',
+            data=pdata,
+            follow_redirects=True
+        )
+
     # Tests Start
     def test_spell_check_get_no_login(self):
         """Tests that spell_check page will redirect to login if a guest user visits."""
@@ -320,6 +333,82 @@ class TestAuth(unittest.TestCase):
         self.assertEqual("1", results.text)
         results = soup.find('a', id="query1")
         self.assertEqual("Query 1", results.text)
+
+    @patch('subprocess.Popen')
+    @patch('tempfile.TemporaryFile', unittest.mock.mock_open(read_data=b'flkfkef\nlkferf\n'))
+    def test_mock_admin_spell_check_history(self, subproc):
+        """Mocks spell check executable and tests that admin view of spell check history renders correctly. Also tests that admin can query for other user's histories."""  # noqa: E501
+        # Register a user
+        response = self.app.get('/register', follow_redirects=True)
+        soup = beautifulsoup(response.data, 'html.parser')
+        csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+        response = self.register(uname='temp1234', pword='temp1234', mfa='1234', csrf_token=csrf_token)
+        self.assertEqual(response.status_code, 200)
+        soup = beautifulsoup(response.data, 'html.parser')
+        results = soup.find_all(id='success')
+        self.assertGreater(len(results), 0, "No flash messages received")
+        self.assertTrue(any("Registration success" in s.text for s in results))
+        # Login as a user
+        csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+        response = self.login(uname='temp1234', pword='temp1234', mfa='1234', csrf_token=csrf_token)
+        self.assertEqual(response.status_code, 200)
+        soup = beautifulsoup(response.data, 'html.parser')
+        results = soup.find_all(id='result')
+        self.assertGreater(len(results), 0, "No flash messages received")
+        self.assertTrue(any("Login success" in s.text for s in results))
+        # Now get the spell_check page
+        response = self.app.get('/spell_check', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        soup = beautifulsoup(response.data, 'html.parser')
+        results = soup.find_all('title')
+        self.assertTrue(any(("Spell Checker - Submission" in s.text) for s in results))
+        self.assertGreater(len(soup.find_all('textarea', id="inputtext")), 0, "No textarea with id 'inputtext' found.")
+        # Setup mocks
+        subproc.return_value = unittest.mock.MagicMock()
+        # Submit some text to spell checker
+        csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+        inputtext = " Some incorrect words "
+        misspelled_words = "flkfkef lkferf"
+        inputtext += misspelled_words
+        response = self.spell_check_text(inputtext=inputtext, csrf_token=csrf_token)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(subproc.called)
+        soup = beautifulsoup(response.data, 'html.parser')
+        results = soup.find('p', id="textout")
+        self.assertEqual(inputtext, results.text.replace("'", ''))
+        results = soup.find('p', id="misspelled")
+        misspelled_words_out = results.text.split(", ")
+        self.assertEqual(len(misspelled_words_out), 2, "Did not return expected amount of misspelled words")
+        for word in misspelled_words_out:
+            self.assertTrue(word in misspelled_words, "Expected misspelled word not returned.")
+        # Logout
+        response = self.logout()
+        self.assertEqual(response.status_code, 200)
+        # Login as admin
+        response = self.app.get('/login', follow_redirects=True)
+        soup = beautifulsoup(response.data, 'html.parser')
+        # Login as default admin
+        csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+        response = self.login(uname='replaceme', pword='replaceme', mfa='1234', csrf_token=csrf_token)
+        self.assertEqual(response.status_code, 200)
+        # Now test that spell check history page stored our word.
+        response = self.app.get('/history', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        soup = beautifulsoup(response.data, 'html.parser')
+        results = soup.find('h3', id="numqueries")
+        self.assertEqual("0", results.text)
+        # Query another User's history
+        csrf_token = soup.find_all('input', id='csrf_token')[0]['value']
+        username_to_query = "temp1234"
+        response = self.query_user_spell_history(userquery=username_to_query, csrf_token=csrf_token)
+        soup = beautifulsoup(response.data, 'html.parser')
+        self.assertEqual(response.status_code, 200)
+        results = soup.find('h3', id="numqueries")
+        self.assertEqual("1", results.text)
+        results = soup.find('a', id="query1")
+        self.assertEqual("Query 1", results.text)
+        results = soup.find('h2')
+        self.assertTrue('temp1234' in results.text)
 
     @patch('subprocess.Popen')
     @patch('tempfile.TemporaryFile', unittest.mock.mock_open(read_data=b'flkfkef\nlkferf\n'))
