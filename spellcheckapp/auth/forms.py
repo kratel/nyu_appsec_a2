c86bfd1df94bfd9984202dcc7ffb3cb48490a1a4
kratel <santi.f.salas@gmail.com>
2019-12-22 22:22:43 -0500

    Update Requirements.

--- spellcheckapp/auth/forms.py
+++ spellcheckapp/auth/forms.py
@@ -1,15 +1,27 @@
 """Defines forms for the Auth module."""
 from flask_wtf import FlaskForm
 
-from wtforms import IntegerField, PasswordField, StringField
+from wtforms import BooleanField, IntegerField, PasswordField, StringField
 from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
-class AuthForm(FlaskForm):
+class RegisterForm(FlaskForm):
     """
-    Auth Form.
+    Register Form.
 
-    This form is used to register a user and also to login.
+    This form is used to register a user.
+    """
+
+    username = StringField(label='Username', id='uname', validators=[DataRequired(), Regexp(regex='^(?=.{5,20}$)[a-zA-Z0-9._]+$', message='Auth failure. Invalid char in username or not between 5 - 20 chars')])  # noqa: E501
+    password = PasswordField(label='Password', id='pword', validators=[DataRequired(), Regexp(regex='^(?=.{8,20}$)[a-zA-Z0-9._$%&*#@!]+$', message='Auth failure. Invalid char in password or not between 8 - 20 chars')])  # noqa: E501
+    mfa = BooleanField(label='2FA', id='2fa', default=False, validators=[Optional()])
+
+
+class LoginForm(FlaskForm):
+    """
+    Login Form.
+
+    This form is used to login as a user.
     """
 
     username = StringField(label='Username', id='uname', validators=[DataRequired(), Regexp(regex='^(?=.{5,20}$)[a-zA-Z0-9._]+$', message='Auth failure. Invalid char in username or not between 5 - 20 chars')])  # noqa: E501
